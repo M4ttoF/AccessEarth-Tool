c9ff8c477cfb96350658588e99887814e85c64ea
Matthew Farias <ezioblade64@gmail.com>
2018-07-10 16:22:01 -0400

    Loops scraper through list of cities now

--- download_json.py
+++ download_json.py
@@ -26,6 +26,8 @@ action=ActionChains(driver)
 
 CITY = "Windsor"
 
+found ={}
+
 
 #Logs into the app
 def login(driver,action):
@@ -47,22 +49,28 @@ def login(driver,action):
 	action.reset_actions()
 	time.sleep(3)	
 
-	searchFor(driver, action)	
-
 
 #Searches for the city on the app
-def searchFor(driver, action):
+def searchFor(driver, action, location):
 
 	elem=driver.find_element_by_name("search")
 	action.move_to_element(elem)
 	action.click()
 	action.perform()
 	action.reset_actions()
-	time.sleep(2)
+	time.sleep(3)
 
 	elem = driver.find_element_by_class_name("searchbar-input")
 
-	elem.send_keys(CITY)
+	name=""
+	name=str(name)
+	for i in range(len(location)):
+		name+= location[i]
+		if i!= location[-1]:
+			name+= ' '
+
+	print(name)
+	elem.send_keys(name)
 	elem=None
 	time.sleep(1)
 	while elem == None:
@@ -71,29 +79,41 @@ def searchFor(driver, action):
 			elem = arr[4]
 		except:
 			time.sleep(1)
+
 	elem.click()
 	time.sleep(2)
-	getNetworkRequests(driver,action)
+	getNetworkRequests(driver,action, location)
 
-#Searches through the network requests and finds the one with the JSON data with locations
-def getNetworkRequests(driver, action):
+#Searches through the network requests and finds the JSON data with locations
+def getNetworkRequests(driver, action, location):
 	script = "var performance = window.performance || window.mozPerformance || window.msPerformance || window.webkitPerformance || {}; var network = performance.getEntries() || {}; return network;"
 	data = driver.execute_script(script)
 	for i in data:
-		if 'factual_data' in i['name']:
-			url = i['name']
-			break
-	downloadJsonLink(url)
+		if 'factual_data' in i['name'] and i['name'] not in found:
+			downloadJsonLink(i['name'], location)
+			found[i['name']] = True
 
 #Goes to URL link and downloads the JSON data to a file named after the city
-def downloadJsonLink(url):
+def downloadJsonLink(url, location):
 	data=urllib.request.urlopen(url)
 	data=data.read()
-	print(data)
-	file = open(CITY + '.JSON', 'w')
+	print("Adding in data for",location)
+	city=""
+	for i in range(len(location)-1):
+		city+= location[i]
+		if i!= location[-2]:
+			city+= ' '
+	city=city[:-1]
+	file = open("Canada\\"+location[-1]+"\\"+city+'.JSON', 'w')
 	file.write(str(data))
 	file.close()
 
 login(driver, action)
+
+searchFile = open("CanadaCities.txt", 'r')
+for line in searchFile:
+	print(line.split())
+	searchFor(driver, action, line.split())
+
 
 driver.close()
